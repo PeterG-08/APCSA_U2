ec76aae8be7d0e1cf1fa0c8ad66b1861fa0723ea
Jack <jackn25@nycstudents.net>
2024-10-15 13:19:02 -0400

    started l8

---
+++ src/L8/Example.java
@@ -0,0 +1,16 @@
+package L8;
+
+public class Example
+{
+    public static void main(String[] args)
+    {
+        int i = 2;
+        double d = 5;
+        System.out.println(i);
+        System.out.println(d);
+        // Print out the min and max values possible for integers
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
+
